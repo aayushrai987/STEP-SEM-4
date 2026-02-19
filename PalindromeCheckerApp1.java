a5d566924c461b103251b4f596892df40ecf02c6
Aayush Rai <aar98117@gmail.com>
2026-02-19 09:34:22 +0530

    Delete PalindromeCheckerApp1.java

--- PalindromeCheckerApp1.java
+++
@@ -1,9 +0,0 @@
-public class PalindromeCheckerApp1 {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome Checker App ");
-        System.out.println("Application Name : Palindrome Checker");
-        System.out.println("Version : 1.0");
-        System.out.println("Use Case :  Application Entry & Welcome Message\n");
-
-    }
-}
