6de5f274ddc5fcd1e1012f1a909f8dbad88a6593
Aayush Rai <aar98117@gmail.com>
2026-03-05 20:54:29 +0530

    UC7: Deque-Based Optimized Palindrome Checker

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -6,35 +6,31 @@ public class PalindromeCheckerApp {
         System.out.print("Enter input: ");
         String input = sc.nextLine();
 
-        int n = input.length();
-
-        Stack<Character> stack = new Stack<>();
-
-        char[] queue = new char[n];
+        char[] deque = new char[input.length()];
         int front = 0;
-        int rear = 0;
+        int rear = -1;
 
-        for (int i = 0; i < n; i++) {
-            char ch = input.charAt(i);
-            stack.push(ch);
-            queue[rear++] = ch;
+        for (char c : input.toCharArray()) {
+            deque[++rear] = c;
         }
 
         boolean isPalindrome = true;
 
-        while (!stack.isEmpty()) {
-            char fromStack = stack.pop();
-            char fromQueue = queue[front++];
-
-            if (fromStack != fromQueue) {
+        while (front < rear) {
+            if (deque[front] != deque[rear]) {
                 isPalindrome = false;
                 break;
             }
+            front++;
+            rear--;
         }
 
         System.out.println("Input : " + input);
         System.out.println("Is Palindrome? : " + isPalindrome);
 
+        sc.close();
+    }
+}
         sc.close();
     }
 }
